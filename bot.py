a2b9904da73dbf98a670beef9862cbf70f496ca9
Luois45 <github@louis45.de>
2023-08-13 16:43:48 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,114 @@
+import pyautogui
+import time
+import virtualkeystroke as vkey
+import keyboard
+import win32api, win32con
+from PIL import Image
+from tqdm import tqdm
+
+
+def click(x, y):
+	win32api.SetCursorPos((x, y))
+	win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, 1, 1, 0, 0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, -1, -1, 0, 0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+	win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+	time.sleep(.01)
+
+
+firstX, firstY = 664, 175
+lastX, lastY = 1254, 765
+openButtonX, openButtonY = 1084, 822
+inputX, inputY = 1081, 740
+closeButtonX, closeButtonY = 1319, 538
+
+diffX = lastX - firstX
+diffY = lastY - firstY
+stepX = diffX / 31
+stepY = diffY / 31
+step = (stepX + stepY) / 2
+
+pixels = {}
+
+imageName = input("Image name:")
+image = Image.open(imageName)
+if image.size[0] != 32 or image.size[1] != 32:
+	print("Resizing image")
+	image = image.resize((32, 32), resample=Image.BOX)
+	image.save(imageName, quality=100)
+imagePixels = image.load()
+for x in range(32):
+	for y in range(32):
+		try:
+			pixels[imagePixels[x, y]].append([x, y])
+		except KeyError:
+			pixels[imagePixels[x, y]] = [[x, y]]
+image.close()
+
+
+def rgb2hex(pixel):
+	return '{:02x}{:02x}{:02x}'.format(pixel[0], pixel[1], pixel[2])
+
+
+def clickPixel(clickX, clickY):
+	click(clickX, clickY)
+
+
+def clickFastPixel(addX, addY):
+	clickX = round(firstX + addX * stepX)
+	clickY = round(firstY + addY * stepY)
+	clickPixel(clickX, clickY)
+
+
+def clickCheckPixel(addX, addY, color, s):
+	clickX = round(firstX + addX * stepX)
+	clickY = round(firstY + addY * stepY)
+	pixelColor = s.getpixel((clickX, clickY))
+	if pixelColor[0:3] == color[0:3]:
+		return False
+	selectColor(color)
+	clickPixel(clickX, clickY)
+	# print(f"{pixelColor[0:3]} -> {color[0:3]}")
+	return True
+
+
+def selectColor(color):
+	hexColor = rgb2hex(color)
+	click(openButtonX, openButtonY)
+	time.sleep(.01)
+	click(inputX, inputY)
+	time.sleep(.01)
+	vkey.typer(string=hexColor)
+	time.sleep(.01)
+	click(closeButtonX, closeButtonY)
+
+
+inputVar = input("Use FastPixel? ")
+
+click(closeButtonX, closeButtonY)
+click(closeButtonX, closeButtonY)
+
+time.sleep(0.1)
+
+if inputVar == "y":
+	for color in tqdm(pixels):
+		selectColor(color)
+		for pixel in pixels[color]:
+			clickFastPixel(pixel[0], pixel[1])
+			if keyboard.is_pressed('q'):
+				quit()
+
+while keyboard.is_pressed('q') == False:
+	s = pyautogui.screenshot()
+	changedPixel = False
+	time.sleep(0.1)
+	for color in tqdm(pixels):
+		for pixel in pixels[color]:
+			if clickCheckPixel(pixel[0], pixel[1], color, s):
+				changedPixel = True
+			if keyboard.is_pressed('q'):
+				quit()
+	if not changedPixel:
+		break
+	click(closeButtonX, closeButtonY)
+	time.sleep(0.1)
